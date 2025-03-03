d71c68fa82c98427e5c247ef1fc354d1ef9274f9
AbrJA <ajaimes@occ.com.mx>
2025-03-03 15:47:29 -0600

    Enhance tests in runtests.jl to improve pool resource management and concurrency checks

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,22 +4,35 @@ import Pools: create, clean!, change!, check
 create(::Type{Int}) = rand(1:10)
 
 @testset "Pools.jl" begin
-    n = 5
+    n = max(2, Threads.nthreads())
     pool = Pool{Int}(n)
 
     @test limit(pool) == n
     @test free(pool) == 0
     @test taken(pool) == 0
 
-    value = acquire!(pool)
-    @test instance(value) isa Int
+    value1 = acquire!(pool)
+    value2 = acquire!(pool)
+    @test instance(value1) isa Int
     @test free(pool) == 0
-    @test taken(pool) == 1
+    @test taken(pool) == 2
 
-    release!(pool, value)
+    release!(pool, value1)
     @test free(pool) == 1
+    @test taken(pool) == 1
+    release!(pool, value2)
+    @test free(pool) == 2
     @test taken(pool) == 0
-    @test_throws ArgumentError release!(pool, value)
+    @test_throws ArgumentError release!(pool, value1)
+
+    Threads.@threads for _ in 1:n
+        withresource(pool) do value
+            sleep(0.1)
+            value
+        end
+    end
+    @test free(pool) == n
+    @test taken(pool) == 0
 
     drain!(pool)
     @test free(pool) == 0
